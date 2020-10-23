184dc6720dffdc130c6afbf2486bf53ab5eecf14
devWemillyCristhiny <devwemilly.frontend@gmail.com>
2020-10-23 09:24:43 -0300

    Meu primeiro Hello World

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Container">
+        <h1>Hello World!</h1>
+      </div>  
+    </div>
+  );
+}
+
+export default App;
